87abb87cd45148d1b862e86a9ceb345315ef4324
alexmortelsison <alexmortelsison@gmail.com>
2025-10-03 21:44:52 +0900

    updated contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,20 +1,51 @@
+"use client";
 import { AiOutlineLinkedin } from "react-icons/ai";
 import { AiFillGithub } from "react-icons/ai";
 import { AiOutlineInstagram } from "react-icons/ai";
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import gsap from "gsap";
 
 export default function ContactPage() {
+  const pRef = useRef(null);
+  const emailRef = useRef(null);
+  const socialRef = useRef(null);
+  useEffect(() => {
+    gsap.to(pRef.current, {
+      opacity: 1,
+      delay: 1,
+      duration: 0.8,
+      ease: "power2.inOut",
+    });
+    gsap.to(emailRef.current, {
+      opacity: 1,
+      delay: 2.5,
+      duration: 0.8,
+      ease: "power2.inOut",
+    });
+    gsap.to(socialRef.current, {
+      opacity: 1,
+      delay: 3.5,
+      duration: 0.8,
+      ease: "power2.inOut",
+    });
+  });
   return (
     <div className="min-h-screen w-full bg-black text-white font-playfair flex justify-center items-center">
       <div className="absolute top-0 left-0 h-full w-28 flex items-center justify-center">
         <h1 className="rotate-90 text-9xl">CONTACT</h1>
       </div>
-      <div className="flex flex-col justify-center text-center space-y-8 tracking-widest items-center">
+      <div
+        className="flex flex-col justify-center text-center space-y-8 tracking-widest items-center opacity-0"
+        ref={pRef}
+      >
         <h1 className=" text-4xl">Let's talk.</h1>
-        <p className="tracking tracking-wide text-7xl hover:underline hover:underline-offset-2 hover:cursor-pointer">
+        <p
+          className="tracking tracking-wide text-7xl hover:underline hover:underline-offset-2 hover:cursor-pointer opacity-0"
+          ref={emailRef}
+        >
           alexmortelsison@gmail.com
         </p>
-        <div className="text-5xl flex space-x-4 pt-4">
+        <div className="text-5xl flex space-x-4 pt-4 opacity-0" ref={socialRef}>
           <AiOutlineInstagram />
           <AiFillGithub />
           <AiOutlineLinkedin />
