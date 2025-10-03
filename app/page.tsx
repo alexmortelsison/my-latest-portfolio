a8b584f1fb65af4c257369c41e258b47f8c32d65
alexmortelsison <alexmortelsison@gmail.com>
2025-10-03 21:52:57 +0900

    updated home

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
-import React, { useEffect } from "react";
-import Hero from "./components/Hero";
+import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 export default function HomePage() {
