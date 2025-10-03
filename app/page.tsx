32be24513de076e9c54f6c057835071cf5cdc42b
alexmortelsison <alexmortelsison@gmail.com>
2025-10-03 21:16:17 +0900

    deleted  intro page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import IntroPage from "./components/Intro";
 import Hero from "./components/Hero";
 
 export default function HomePage() {
   return (
     <div>
-      <IntroPage />
+      <Hero />
     </div>
   );
 }
