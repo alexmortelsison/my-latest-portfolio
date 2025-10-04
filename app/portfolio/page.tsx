62ec88437b8dc4f29774364b2c5896b5253852c5
alexmortelsison <alexmortelsison@gmail.com>
2025-10-04 14:13:18 +0900

    updated portfolio

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -2,6 +2,7 @@
 import { AiOutlineArrowRight } from "react-icons/ai";
 import gsap from "gsap";
 import React, { useEffect, useRef } from "react";
+import Link from "next/link";
 
 export default function PortfolioPage() {
   const pRef = useRef(null);
@@ -11,6 +12,7 @@ export default function PortfolioPage() {
   const p1 = useRef(null);
   const p2 = useRef(null);
   const p3 = useRef(null);
+  const p4 = useRef(null);
   useEffect(() => {
     gsap.to(pRef.current, {
       opacity: 1,
@@ -57,9 +59,16 @@ export default function PortfolioPage() {
       duration: 4,
       ease: "power2.inOut",
     });
+    gsap.to(p4.current, {
+      opacity: 1,
+      x: 12,
+      delay: 0.5,
+      duration: 4,
+      ease: "power2.inOut",
+    });
   }, []);
   return (
-    <div className="min-h-screen w-full bg-black text-white font-playfair flex justify-center items-center tracking-widest relative">
+    <div className="min-h-screen w-full bg-black text-white font-playfair flex justify-center items-center tracking-widest relative overflow-hidden">
       <div
         className="absolute top-0 left-0 h-full w-28 flex items-center justify-center -translate-x-50"
         ref={contactRef}
@@ -69,42 +78,62 @@ export default function PortfolioPage() {
         </h1>
       </div>
       <div className="flex flex-col tracking-widest lg:w-screen md:pl-28 px-8 h-full pt-48 space-y-8">
-        <div className="pl-24 h-[80px] border-b px-8 items-center cursor-pointer transition-colors duration-300 flex">
-          <p className="text-3xl text-[#1dd1d8]">01</p>
-          <div
-            className="flex justify-between flex-1 ml-4 -translate-x-160 opacity-0"
-            ref={p1}
-          >
-            <h1 className="text-4xl">Portfolio</h1>
-            <p className="text-2xl">
-              <AiOutlineArrowRight />
-            </p>
+        <Link href={"/"}>
+          <div className="pl-24 h-[80px] border-b px-8 items-center cursor-pointer transition-colors duration-300 flex">
+            <p className="text-3xl text-[#1dd1d8]">01</p>
+            <div
+              className="flex justify-between flex-1 ml-4 -translate-x-160 opacity-0"
+              ref={p1}
+            >
+              <h1 className="text-4xl">Portfolio</h1>
+              <p className="text-2xl">
+                <AiOutlineArrowRight />
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="pl-24 h-[80px] border-b px-8 items-center cursor-pointer transition-colors duration-300 flex">
-          <p className="text-3xl text-[#1dd1d8]">02</p>
-          <div
-            className="flex justify-between flex-1 ml-4 -translate-x-160 opacity-0"
-            ref={p2}
-          >
-            <h1 className="text-4xl">Apple Clone</h1>
-            <p className="text-2xl">
-              <AiOutlineArrowRight />
-            </p>
+        </Link>
+        <a href="https://apple-clone-drmort.vercel.app/" target="_blank">
+          <div className="pl-24 h-[80px] border-b px-8 items-center cursor-pointer transition-colors duration-300 flex">
+            <p className="text-3xl text-[#1dd1d8]">02</p>
+            <div
+              className="flex justify-between flex-1 ml-4 -translate-x-160 opacity-0"
+              ref={p2}
+            >
+              <h1 className="text-4xl">Apple Clone</h1>
+              <p className="text-2xl">
+                <AiOutlineArrowRight />
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="pl-24 h-[80px] border-b px-8 items-center cursor-pointer transition-colors duration-300 flex">
-          <p className="text-3xl text-[#1dd1d8]">03</p>
-          <div
-            className="flex justify-between flex-1 ml-4 -translate-x-160 opacity-0"
-            ref={p3}
-          >
-            <h1 className="text-4xl">On.com Clone</h1>
-            <p className="text-2xl">
-              <AiOutlineArrowRight />
-            </p>
+        </a>
+        <a href="https://on-clone.vercel.app/" target="_blank">
+          <div className="pl-24 h-[80px] border-b px-8 items-center cursor-pointer transition-colors duration-300 flex">
+            <p className="text-3xl text-[#1dd1d8]">03</p>
+            <div
+              className="flex justify-between flex-1 ml-4 -translate-x-160 opacity-0"
+              ref={p3}
+            >
+              <h1 className="text-4xl">On.com Clone</h1>
+              <p className="text-2xl">
+                <AiOutlineArrowRight />
+              </p>
+            </div>
           </div>
-        </div>
+        </a>
+        <a href="https://djgos.vercel.app/" target="_blank">
+          <div className="pl-24 h-[80px] border-b px-8 items-center cursor-pointer transition-colors duration-300 flex">
+            <p className="text-3xl text-[#1dd1d8]">04</p>
+            <div
+              className="flex justify-between flex-1 ml-4 -translate-x-160 opacity-0"
+              ref={p4}
+            >
+              <h1 className="text-4xl">DJ GOS</h1>
+              <p className="text-2xl">
+                <AiOutlineArrowRight />
+              </p>
+            </div>
+          </div>
+        </a>
       </div>
     </div>
   );
