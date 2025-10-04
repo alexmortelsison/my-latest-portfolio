2126a45260199de4d55c81ce5ee014662242620c
alexmortelsison <alexmortelsison@gmail.com>
2025-10-04 09:12:57 +0900

    updated contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -30,7 +30,7 @@ export default function ContactPage() {
       ease: "power2.inOut",
     });
     gsap.to(contactRef.current, {
-      x: 0,
+      x: 23,
       delay: 0.5,
       duration: 0.8,
       ease: "power2.inOut",
@@ -39,26 +39,28 @@ export default function ContactPage() {
   return (
     <div className="min-h-screen w-full bg-black text-white font-playfair flex justify-center items-center tracking-widest">
       <div
-        className="absolute top-0 left-0 h-full w-28 flex items-center justify-center -translate-x-40"
+        className="absolute top-0 left-0 h-full w-28 flex items-center justify-center -translate-x-50"
         ref={contactRef}
       >
-        <h1 className="rotate-90 text-9xl">CONTACT</h1>
+        <h1 className="rotate-90 text-[180px] pr-8">CONTACT</h1>
       </div>
-      <div
-        className="flex flex-col justify-center text-center space-y-8 tracking-widest items-center opacity-0"
-        ref={pRef}
-      >
-        <h1 className=" text-4xl tracking-wider">Let's talk.</h1>
-        <p
-          className="tracking tracking-wide text-7xl hover:underline hover:underline-offset-2 hover:cursor-pointer opacity-0"
-          ref={emailRef}
-        >
-          alexmortelsison@gmail.com
-        </p>
-        <div className="text-5xl flex space-x-4 pt-4 opacity-0" ref={socialRef}>
-          <AiOutlineInstagram />
-          <AiFillGithub />
-          <AiOutlineLinkedin />
+      <div className="flex-col justify-center text-center tracking-widest items-center lg:w-screen pl-28">
+        <div className="opacity-0" ref={pRef}>
+          <h1 className="lg:text-4xl text-xl tracking-wider">Let's talk.</h1>
+          <p
+            className="tracking tracking-wide lg:text-7xl text-2xl hover:underline hover:underline-offset-2 hover:cursor-pointer opacity-0 pt-8"
+            ref={emailRef}
+          >
+            alexmortelsison@gmail.com
+          </p>
+          <div
+            className="lg:text-5xl text-3xl flex space-x-4 opacity-0 text-center justify-center pt-8"
+            ref={socialRef}
+          >
+            <AiOutlineInstagram />
+            <AiFillGithub />
+            <AiOutlineLinkedin />
+          </div>
         </div>
       </div>
     </div>
