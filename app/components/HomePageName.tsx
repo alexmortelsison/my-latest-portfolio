ef6b98e90c2cdb4cd25eccd297cded328eb00061
alexmortelsison <alexmortelsison@gmail.com>
2025-10-25 12:53:22 +0900

    updated homepage design

--- app/components/HomePageName.tsx
+++ app/components/HomePageName.tsx
@@ -1,6 +1,13 @@
-"use client";
+import { BiRightArrow } from "react-icons/bi";
+import { BiLeftArrow } from "react-icons/bi";
+import { AiOutlineArrowRight } from "react-icons/ai";
+import { AiOutlineMail } from "react-icons/ai";
+import { AiFillGithub } from "react-icons/ai";
+import { AiOutlineInstagram } from "react-icons/ai";
+
 import React from "react";
 import { useScramble } from "use-scramble";
+import Link from "next/link";
 
 export default function HomePageName() {
   const nameRef = useScramble({
@@ -24,20 +31,39 @@ export default function HomePageName() {
     range: [65, 76, 69, 88, 83, 73, 79, 78],
     overdrive: false,
   });
+  const frontendRef = useScramble({
+    text: "Frontend Developer | Web Designer",
+    playOnMount: true,
+    speed: 0.2,
+    range: [65, 76, 69, 88, 83, 73, 79, 78],
+    overdrive: false,
+  });
+  const projectRef = useScramble({
+    text: "Dior. Apple. On. DJ Gos. Toyota",
+    playOnMount: true,
+    speed: 0.2,
+    range: [65, 76, 69, 88, 83, 73, 79, 78],
+    overdrive: false,
+  });
+
   const scrambleText = () => {
     nameRef.replay();
     surnameRef.replay();
   };
   return (
     <div
-      className="text-[180px] text-white z-50 absolute hover:cursor-pointer"
+      className="text-7xl text-white z-50 absolute hover:cursor-pointer "
       onMouseEnter={scrambleText}
     >
       <div className="">
         <div>
-          <p className="fixed text-sm right-78 font-mono">
+          <p className="fixed top-48 text-sm xl:right-78 font-mono flex items-center">
+            <AiOutlineArrowRight className="text-red-500 text-2xl mr-4" />
             From{" "}
-            <span className="text-red-500" ref={japanRef.ref}>
+            <span
+              className="text-red-500 mx-1 font-semibold"
+              ref={japanRef.ref}
+            >
               Japan
             </span>{" "}
             <span className="underline">to anywhere in the world.</span>
@@ -46,13 +72,34 @@ export default function HomePageName() {
       </div>
       <span ref={nameRef.ref} className=" font-overcamebold" />
       <span className="font-overcameoutline" ref={surnameRef.ref} />
-      <span className="font-sans text-[150px]">/</span>
+      <span className="font-sans text-red-500">/.</span>
       <div>
-        <p className="text-lg font-mono">Frontend Developer | Web Designer</p>
+        <p className="text-sm font-mono" ref={frontendRef.ref}></p>
+        <div className="flex items-center space-x-4 text-4xl mt-16">
+          <BiRightArrow className="text-red-500" />
+          <a href="https://www.instagram.com/alexismortelsison" target="_blank">
+            <AiOutlineInstagram />
+          </a>
+          <a href="https://github.com/alexmortelsison" target="_blank">
+            <AiFillGithub />
+          </a>
+          <AiOutlineMail />
+          <BiLeftArrow className="text-red-500" />
+        </div>
+        <div
+          className="flex items-center space-x-1 font-mono text-sm mt-24"
+          ref={projectRef.ref}
+        >
+          <Link href="/">Dior.</Link>
+          <Link href="/">Apple.</Link>
+          <Link href="/">On.</Link>
+          <Link href="/">DJ Gos.</Link>
+          <Link href="/">Toyota.</Link>
+        </div>
       </div>
-      <div className="fixed right-80 pt-16 font-mono text-gray-300">
+      <div className="fixed right-80 xl:pt-8 font-mono text-gray-300">
         <div>
-          <p className="text-xl text-red-500 font-black">./</p>
+          <p className="text-xl text-red-500 font-black">#</p>
           <p className=" text-red-500 text-xl font-semibold">News</p>
           <p className="text-lg">2025</p>
           <div className="flex items-center text-sm space-x-2 text-gray-500">
--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,7 @@ export default function Home() {
       <div className="bg-gradient-to-b from-black via-black/70 to-black/80 inset-0 absolute z-0"></div>
 
       <div className="">
-        <video src={"/dior.mp4"} muted autoPlay playsInline loop />
+        <video src={"/bg.mp4"} muted autoPlay playsInline loop className="" />
       </div>
 
       <div className="flex justify-start items-center z-0 absolute inset-0 max-w-7xl mx-auto">
