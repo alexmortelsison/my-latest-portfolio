cd59a9b123a7c3b559dca784b38218818c27b886
alexmortelsison <alexmortelsison@gmail.com>
2025-10-27 09:07:14 +0900

    updated intro text

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -6,14 +6,14 @@ export default function Intro() {
   const router = useRouter();
   const delay = 1500;
   const firstRef = useScramble({
-    text: "ALEX",
-    speed: 0.1,
+    text: "Take your website",
+    speed: 0.2,
     overdrive: false,
     range: [65, 76, 69, 88, 83, 73, 79, 78],
   });
   const surnameRef = useScramble({
-    text: "SISON",
-    speed: 0.1,
+    text: "to the next level",
+    speed: 0.2,
     overdrive: false,
     range: [65, 76, 69, 88, 83, 73, 79, 78],
     onAnimationEnd: () => {
@@ -24,13 +24,13 @@ export default function Intro() {
   });
 
   return (
-    <div className="flex text-9xl">
+    <div className="xl:flex justify-center flex flex-col items-center text-center text-7xl space-x-4">
       <h1
-        className="text-white font-overcamebold text-7xl md:text-9xl"
+        className="text-white font-overcamebold text-4xl md:text-7xl"
         ref={firstRef.ref}
       ></h1>
       <span
-        className="text-red-500 font-overcameoutline text-7xl md:text-9xl"
+        className="text-red-500 font-overcameoutline text-4xl md:text-7xl"
         ref={surnameRef.ref}
       ></span>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,10 @@ import Intro from "./components/Intro";
 
 export default function Home() {
   return (
-    <div className="bg-gradient-to-b from-black via-black/80 to-black h-[100vh] flex justify-center items-center text-9xl">
-      <Intro />
+    <div className="bg-gradient-to-b from-black via-black/80 to-black h-[100vh] flex justify-center items-center">
+      <div className="max-w-7xl mx-auto flex items-center xl:whitespace-nowrap">
+        <Intro />
+      </div>
     </div>
   );
 }
